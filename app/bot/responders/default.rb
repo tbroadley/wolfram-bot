76af20ce885c88edf90eab00ef5131584398e4a5
tbroadley <buriedunderbooks@hotmail.com>
2016-05-04 01:27:56 +0100

    updated default responder

--- app/bot/responders/default.rb
+++ app/bot/responders/default.rb
@@ -1,14 +1,14 @@
 module Responders
   class Default < ApplicationResponder
     respond_to "scan-data", "text", "link", "picture", "video", "sticker"
-    #
-    # def can_handle?
-    #   true
-    # end
-    #
-    # def handle
-    #   text_response('Hello!')
-    # end
-    #
+
+    def can_handle?
+      true
+    end
+
+    def handle
+      text_response("Sorry, I don't know how to respond to that.")
+    end
+
   end
 end
---
+++ app/bot/responders/wolfram.rb
@@ -0,0 +1,14 @@
+require "wolfram-alpha"
+
+module Responders
+  class Default < ApplicationResponder
+    def can_handle?
+      true
+    end
+
+    def handle
+            
+    end
+
+  end
+end
